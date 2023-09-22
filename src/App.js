38acd4cadcc8b78d61b3a67f17f1c657df66f006
Samita Mondal <samitam@acquiscompliance.com>
2023-09-22 15:10:31 +0530

    Add new branding

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import BarChart from './BarChart/BarChart';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
+import BarChart from "./BarChart/BarChart";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <div className='d-flex justify-content-space-around align-items-center vh-100'>
-          <div className='card'>
-            <div className='card-header'>Horizontal Bar Chart 1</div>
-            <div className='card-body'><BarChart id="bar1" dataset="4, 6, 3, 10, 7" /></div>
+        <div className="d-flex justify-content-space-around align-items-center vh-100">
+          <div className="card">
+            <div className="card-header">Horizontal Bar Chart 1</div>
+            <div className="card-body">
+              <BarChart id="bar1" dataset="4, 6, 3, 10, 7" />
+            </div>
           </div>
-          <div className='card'>
-            <div className='card-header'>Horizontal Bar Chart 2</div>
-            <div className='card-body'><BarChart id="bar2" dataset="10, 8, 3, 6, 2, 5" /></div>
+          <div className="card">
+            <div className="card-header">Horizontal Bar Chart 2</div>
+            <div className="card-body">
+              <BarChart id="bar2" dataset="10, 8, 3, 6, 2, 5" />
+            </div>
           </div>
         </div>
       </div>
--- src/BarChart/BarChart.js
+++ src/BarChart/BarChart.js
@@ -1,70 +1,99 @@
-import React from 'react';
-import * as d3 from 'd3';
+import React from "react";
+import * as d3 from "d3";
 
 class barChart extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    horizontalChart(dataset) {
-        var data = dataset;
-        var width = 300;
-        var height = 300;
-        var scaleFactor = 20;
-        var barHeight = 30;
+  horizontalChart(dataset) {
+    var data = dataset;
+    var width = 300;
+    var height = 300;
+    var scaleFactor = 20;
+    var barHeight = 30;
 
-        // var colors = d3.scaleOrdinal(d3.schemeAccent);
-        var colors = d3.scaleSequential().domain([1,10]).interpolator(d3.interpolateViridis);
+    // var colors = d3.scaleOrdinal(d3.schemeAccent);
+    var colors = d3
+      .scaleSequential()
+      .domain([1, 10])
+      .interpolator(d3.interpolateViridis);
 
-        var container = d3.select('.bar-chart.' + this.props.id);
+    var container = d3.select(".bar-chart." + this.props.id);
 
-        var graph = container.append("svg")
-            .attr("width", width)
-            .attr("height", barHeight * data.length);
+    var graph = container
+      .append("svg")
+      .attr("width", width)
+      .attr("height", barHeight * data.length);
 
-        var barHorizontal = graph.selectAll('g')
-            .data(data)
-            .enter()
-            .append("g")
-            .attr("transform", function(d, i) {
-                return "translate(0," + (i * barHeight) + ")";
-            });
+    var barHorizontal = graph
+      .selectAll("g")
+      .data(data)
+      .enter()
+      .append("g")
+      .attr("transform", function (d, i) {
+        return "translate(0," + i * barHeight + ")";
+      });
 
-        barHorizontal.append("rect")
-            .attr("width", function(d) {
-                return (d * scaleFactor);
-            })
-            .attr("height", barHeight - 10)
-            .attr("rx", 2)
-            .attr("fill", function(d, i) {
-                return colors(d);
-            });
+    const x = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.frequency)])
+      .range([0, width - 0]);
 
-        barHorizontal.append("text")
-            .attr("x", function(d) {
-                return (d * scaleFactor) + 5;
-            })
-            .attr("y", (barHeight - 10) / 2)
-            .attr("dy", ".35em")
-            .style("font-size", "12px")
-            .style("color", "#FFF")
-            .text(function(d) { return d; });
-    }
+    const y = d3
+      .scaleBand()
+      .domain(d3.sort(data, (d) => -d.frequency))
+      .rangeRound([0, height - 0])
+      .padding(0.1);
 
-    componentDidMount() {
-        let dataset = this.props.dataset.split(",");
-        dataset.forEach((e,i) => {
-            dataset[i] = parseInt(e);
-        });
+    barHorizontal
+      .append("rect")
+      .attr("width", function (d) {
+        return d * scaleFactor;
+      })
+      .attr("height", barHeight - 10)
+      .attr("rx", 2)
+      .attr("fill", function (d, i) {
+        return colors(d);
+      });
 
-        this.horizontalChart(dataset);
-    }
+    barHorizontal
+      .append("text")
+      .attr("x", function (d) {
+        return d * scaleFactor + 5;
+      })
+      .attr("y", (barHeight - 10) / 2)
+      .attr("dy", ".35em")
+      .style("font-size", "12px")
+      .style("color", "#FFF")
+      .text(function (d) {
+        return "name " + d;
+      });
 
-    render() {
-        return (
-            <div className={"bar-chart " + this.props.id}></div>
-        );
-    }
+    barHorizontal
+      .append("g")
+      .attr("transform", `translate(0, 15)`)
+      .call(d3.axisTop(x).ticks(width / 80))
+      .call((g) => g.select(".domain").remove());
+
+    barHorizontal
+      .append("g")
+      .attr("transform", `translate(20 ,-15)`)
+      .call(d3.axisLeft(y).tickSizeOuter(0));
+  }
+
+  componentDidMount() {
+    let dataset = this.props.dataset.split(",");
+    dataset.forEach((e, i) => {
+      dataset[i] = parseInt(e);
+    });
+
+    this.horizontalChart(dataset);
+  }
+
+  render() {
+    return <div className={"bar-chart " + this.props.id}></div>;
+  }
 }
 
 export default barChart;
